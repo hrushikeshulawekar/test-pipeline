d48347f9cef6695cf8e3deb9e1fb14e169816b0e
hrushikeshulawekar <hrushikesh.s.ulawekar@gmail.com>
2026-02-24 15:56:12 +0530

    lets call it a day.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          It is for test.
+          Lets call it a day.
         </p>
         <a
           className="App-link"
