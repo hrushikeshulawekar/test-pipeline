499de9b3438ced561f996fca790aedabf0278fbb
hrushikeshulawekar <hrushikesh.s.ulawekar@gmail.com>
2026-02-24 16:28:26 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Lets confirm it.
+          Ok it is enough for today.
         </p>
         <a
           className="App-link"
