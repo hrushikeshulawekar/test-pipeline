500202be7efc5f8b2c685f6fc2724fbf775f7910
hrushikeshulawekar <hrushikesh.s.ulawekar@gmail.com>
2026-02-24 11:56:05 +0530

    first test update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          It is for test.
         </p>
         <a
           className="App-link"
